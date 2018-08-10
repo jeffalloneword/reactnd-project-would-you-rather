5a4ccc2d4901c5ff9c3614493a33c3bcbbfac169
Jeff Hyatt <jeffalloneword@users.noreply.github.com>
2018-08-09 20:47:40 -0400

    cleanup nav css spacing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-export default function Nav () {
+export default function Nav() {
   return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new' activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leaderboard
-          </NavLink>
-        </li>
-        <li>
-          <div className='right'></div>
-        </li>
-        <li>
-          Hello, Username
-          {/* TODO: codify username in nav bar */}
-        </li>
-        <li>
-          <NavLink to='/signin' activeClassName='active'>
-            Logout
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="nav">
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new" activeClassName="active">
+              New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/leaderboard" activeClassName="active">
+              Leaderboard
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div> </div>
+      <div>
+        <ul>
+          <li>
+            Hello, Username
+            {/* TODO: codify username in nav bar */}
+          </li>
+          <li>
+            <NavLink to="/signin" activeClassName="active">
+              Logout
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
