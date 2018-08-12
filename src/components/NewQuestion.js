84d06706524663151ae0a7a13aa10f62346af590
Jeff Hyatt <jeffalloneword@users.noreply.github.com>
2018-08-12 17:10:36 -0400

    setup new question component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
 
 class NewQuestion extends Component {
+  state = {
+    textOptionOne: '',
+  }
+  handleChange = e => {
+    const textOptionOne = e.target.value
+
+    this.setState(() => ({
+      textOptionOne,
+    }))
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const { textOptionOne } = this.state
+
+    // todo: add question to store
+
+    console.log('New Question: ', textOptionOne)
+
+    this.setState(() => ({
+      textOptionOne: '',
+    }))
+  }
   render() {
+    const { textOptionOne } = this.state
+
     return (
-      <div className='myCssClass'>
-        New Question
+      <div className="myCssClass">
+        <h3 className="center">Create New Question</h3>
+        <form className="" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="Option 1"
+            value={textOptionOne}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={120}
+          />
+        </form>
       </div>
     )
   }
