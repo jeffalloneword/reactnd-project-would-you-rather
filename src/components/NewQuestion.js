57645db80cd6e1aa19b602c69a22b87af40d91a6
Jeff Hyatt <jeffalloneword@users.noreply.github.com>
2018-08-13 21:51:30 -0400

    setup new question option inputs

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -3,41 +3,60 @@ import React, { Component } from 'react'
 class NewQuestion extends Component {
   state = {
     textOptionOne: '',
+    textOptionTwo: '',
   }
   handleChange = e => {
-    const textOptionOne = e.target.value
-
-    this.setState(() => ({
-      textOptionOne,
-    }))
+    e.persist()
+    const optionKey = e.target.name
+    const optionValue = e.target.value
+    console.log('input: ', optionKey, optionValue)
+    this.setState({ [optionKey]: optionValue })
   }
   handleSubmit = e => {
     e.preventDefault()
 
-    const { textOptionOne } = this.state
+    const { textOptionOne, textOptionTwo } = this.state
 
     // todo: add question to store
 
-    console.log('New Question: ', textOptionOne)
+    console.log('New Question: ', this.state)
 
     this.setState(() => ({
       textOptionOne: '',
+      textOptionTwo: '',
     }))
   }
   render() {
-    const { textOptionOne } = this.state
+    const { textOptionOne, textOptionTwo } = this.state
+    console.log('render: ', textOptionOne, textOptionTwo)
 
     return (
-      <div className="myCssClass">
+      <div className="new-question">
         <h3 className="center">Create New Question</h3>
-        <form className="" onSubmit={this.handleSubmit}>
+        <form className="form-question" onSubmit={this.handleSubmit}>
           <textarea
+            name="textOptionOne"
             placeholder="Option 1"
             value={textOptionOne}
             onChange={this.handleChange}
-            className="textarea"
+            className="textarea-question"
             maxLength={120}
           />
+          <textarea
+            name="textOptionTwo"
+            placeholder="Option 2"
+            value={textOptionTwo}
+            onChange={this.handleChange}
+            className="textarea-question"
+            maxLength={120}
+          />
+          <button
+            className="btn-question"
+            type="submit"
+            disabled={textOptionOne === ''}
+          >
+            Submit
+          </button>
         </form>
       </div>
     )
